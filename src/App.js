984e5039666b8a81eae3e03838613753c829ee13
sage-jordan <sagemjordan@gmail.com>
2020-01-29 11:50:01 -0500

    Changed from demo key to personalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import  React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import PhotoOfTheDay from "./components/Card";
 
+export default function App() {
+  const [data, setData] = useState({});
 
-export default function App () {
-    const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=wsIfZQBWeTafkOWECuHwkAJ60NVRXblh3bZMvibg"
+      )
+      .then(res => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        axios
-            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-            .then(res => {
-              console.log(res)
-              setData(res.data)
-            })
-            .catch(err => console.log(err));
-    }, []);
-
-    return (
-        <div className="App">
-            <PhotoOfTheDay props={data}/>
-        </div>
-    );
-};
+  return (
+    <div className="App">
+      <PhotoOfTheDay props={data} />
+    </div>
+  );
+}
